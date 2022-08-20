30e29f8e4ebba8f211d4aa92c3f40f410eccb24d
Dhananjay301996 <gadhavedhananjay123@gmail.com>
2022-08-20 18:40:18 +0530

    Last name regex code with lambda expression

--- Day23Assignment/LambdaExpression.java
+++ Day23Assignment/LambdaExpression.java
@@ -1,18 +1,26 @@
 package Day23Assignment;
 import java.util.regex.Pattern;
 
+/**
+ * Regex program code with help of Lambda expressions
+ * @author HP
+ *
+ */
 public class LambdaExpression {
 
 	public static void main(String[] args) {
+/*
+ *Last name 
+ */
 		System.out.println("Welcome to User Registration program");
-		Regex firstName = (name) -> {
+		Regex LastName = (name) -> {
 			boolean check = Pattern.matches("^([A-Z]{1}+)[A-Za-z]{2,}$", name);
 			if (check == true) {
 				System.out.println("Firstname is Valid");
 			} else
 				System.out.println("Enter valid name");
 		};
-		firstName.validName("Dhananjay");
+		LastName.validName("Dhananjay");
 	}
 
 	}
